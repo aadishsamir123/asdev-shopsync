e44ebb00bd7faca7aeb4d65eeec3814b61011254
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2025-04-14 17:38:48 +0800

    "Removed" theme_notifier.dart

--- lib/services/theme_notifier.dart
+++ lib/services/theme_notifier.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class ThemeNotifier extends ChangeNotifier {
-  final String key = "darkMode";
-  SharedPreferences? _prefs;
-  bool _isDarkMode = false;
-
-  bool get isDarkMode => _isDarkMode;
-
-  ThemeNotifier() {
-    _loadFromPrefs();
-  }
-
-  toggleTheme() {
-    _isDarkMode = !_isDarkMode;
-    _saveToPrefs();
-    notifyListeners();
-  }
-
-  _initPrefs() async {
-    _prefs ??= await SharedPreferences.getInstance();
-  }
-
-  _loadFromPrefs() async {
-    await _initPrefs();
-    _isDarkMode = _prefs?.getBool(key) ?? false;
-    notifyListeners();
-  }
-
-  _saveToPrefs() async {
-    await _initPrefs();
-    _prefs?.setBool(key, _isDarkMode);
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// class ThemeNotifier extends ChangeNotifier {
+//   final String key = "darkMode";
+//   SharedPreferences? _prefs;
+//   bool _isDarkMode = false;
+//
+//   bool get isDarkMode => _isDarkMode;
+//
+//   ThemeNotifier() {
+//     _loadFromPrefs();
+//   }
+//
+//   toggleTheme() {
+//     _isDarkMode = !_isDarkMode;
+//     _saveToPrefs();
+//     notifyListeners();
+//   }
+//
+//   _initPrefs() async {
+//     _prefs ??= await SharedPreferences.getInstance();
+//   }
+//
+//   _loadFromPrefs() async {
+//     await _initPrefs();
+//     _isDarkMode = _prefs?.getBool(key) ?? false;
+//     notifyListeners();
+//   }
+//
+//   _saveToPrefs() async {
+//     await _initPrefs();
+//     _prefs?.setBool(key, _isDarkMode);
+//   }
+// }
